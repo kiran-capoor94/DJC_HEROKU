99d085c50b1fb4f0f77a679b027fa9ee10efb843
Kiran Capoor <kiran.capoor94@gmail.com>
2019-01-04 01:20:34 +0530

    Click

--- rejuvahome/apps/gallery/views.py
+++ rejuvahome/apps/gallery/views.py
@@ -6,14 +6,16 @@ from django.views.generic import ListView, DetailView
 from .models import Gallery
 from apps.tags.models import Tag
 
+
 class GalleryListView(ListView):
     template_name = 'gallery/list.html'
 
     def get_context_data(self, *args, **kwargs):
-        context = super(GalleryListView, self).get_context_data(*args, **kwargs)
+        context = super(GalleryListView, self).get_context_data(
+            *args, **kwargs)
         # context['title'] = 'Rejuva Aesthetica | Gallery'
         return context
-    
+
     def get_queryset(self, *args, **kwargs):
         request = self.request
         return Gallery.objects.all()
