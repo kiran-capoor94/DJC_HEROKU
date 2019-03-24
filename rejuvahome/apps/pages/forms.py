4f7ee800f716754531da732712952a2df6f666e7
Kiran Deepak Capoor <kiran.capoor94@gmail.com>
2019-03-24 13:21:33 +0530

    Req fields

--- rejuvahome/apps/pages/forms.py
+++ rejuvahome/apps/pages/forms.py
@@ -4,15 +4,22 @@ from django import forms
 class ContactForm(forms.Form):
     full_name = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control',
-        'placeholder': 'Full Name'}))
+        'placeholder': 'Full Name',
+        'autocmplete': 'name'}),
+        required=True)
     email = forms.EmailField(widget=forms.EmailInput(attrs={
         'class': 'form-control',
-        'placeholder': 'Email'}))
+        'placeholder': 'Email',
+        'autocomplete': 'email'}),
+        required=True)
     phone_number = forms.IntegerField(widget=forms.NumberInput(attrs={
         'class': 'form-control',
-        'placeholder': 'Phone Number'}))
+        'placeholder': 'Phone Number',
+        'autocomplete': 'number'}),
+        required=True)
     content = forms.CharField(widget=forms.Textarea(attrs={
         'class': 'form-control',
         'placeholder': 'Message',
         'cols': '40',
-        'rows': '10'}))
+        'rows': '10'}),
+        required=True)
